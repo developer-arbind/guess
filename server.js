26b454c0f31903453b8dfc043248da4e712dc144
Arbind Kumar Mahato <you@example.com>
2024-05-03 06:30:32 +0545

    changes

---
+++ server.js
@@ -0,0 +1,61 @@
+let express = require("express");
+let http = require("http");
+let bodyParser = require("body-parser");
+
+let app = express();
+
+let cors = require("cors");
+const mongodb = require("mongoose");
+let db;
+try {
+  (async () => {
+    db = await mongodb.connect(
+      "mongodb+srv://everywhere:Arbnd2064@cluster0.jnekl3m.mongodb.net/guesss"
+    );
+  })();
+} catch (err) {
+  return err;
+}
+let schema = new mongodb.Schema({
+  name: String,
+  time: String,
+});
+let proper = new mongodb.Schema({
+  name: String,
+});
+let schemaModel = mongodb.model("names", schema);
+let properModel = mongodb.model("proper", proper);
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+const server = http.createServer(app);
+
+app.get("/:name", async (Req, Res) => {
+  const name = Req.params.name;
+  console.log("name: ", name);
+  let names = await properModel.find({});
+  if (names.length > 1) {
+    return Res.status(400).json({
+      message: "you can't view this again, sorry",
+    });
+  }
+  let data = new schemaModel({
+    name,
+    time: new Date().toISOString(),
+  });
+  await data.save();
+  if (names[0].name === name.toLowerCase()) {
+    const data = new properModel({
+      name,
+    });
+    await data.save();
+    return Res.status(200).json({
+      message: "",
+    });
+  }
+  return Res.status(404).json({
+    message: "wrong guess",
+  });
+});
+
+server.listen(8000, () => console.log("server listening on port 8000"));
